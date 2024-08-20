1fd772b5b020ec1e410eb0178613820cfdd20697
Alicemarkaryan <alisa.markaryan@yahoo.com>
2024-08-20 18:28:58 +0400

    .

--- KomitasPark/KomitasParkDAL/Entites/BaseEntity.cs
+++ KomitasPark/KomitasParkDAL/Entites/BaseEntity.cs
@@ -6,6 +6,7 @@
         public string? Name { get; set; }
         public string? Code { get; set; }
 
+
         public BaseEntity(int id, string name, string code)
         {
             this.Id = id;
